199e811c88a3a399f93f978442443466fa69e902
Lianqing Qu <lianqing.qu@rightcapital.com>
2026-02-14 14:54:25 +0800

    Dismiss keyboard on tap outside input fields

--- OurFamilyLedger/App/OurFamilyLedgerApp.swift
+++ OurFamilyLedger/App/OurFamilyLedgerApp.swift
@@ -68,6 +68,12 @@ struct OurFamilyLedgerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onTapGesture {
+                    UIApplication.shared.sendAction(
+                        #selector(UIResponder.resignFirstResponder),
+                        to: nil, from: nil, for: nil
+                    )
+                }
         }
         .modelContainer(modelContainer)
     }
